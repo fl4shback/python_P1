0f2c4f6af33c3358444d6817dbec5a44e953cba9
fl4shback <fl4shback@outlook.com>
2021-11-03 23:21:51 +0100

    improved comments
    prevents overriding default python functions lol

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 print("Hello!")
-var =  "test"
-int = 12
-float = 12.5
-bool = True
-print(var[4])
-print(type(var))
-print(int)
-print(type(int))
-print(float)
-print(type(float))
-print(bool)
-print(type(bool))
+string_var =  "test"
+int_var = 12
+float_var = 12.5
+bool_var = True
+print(string_var[3]) #prints only 4th character (0,1,2,3) from var
+print(type(string_var))
+print(int_var)
+print(type(int_var))
+print(float_var)
+print(type(float_var))
+print(bool_var)
+print(type(bool_var))
+
+if(type(bool_var) == str):
+    print("success")
+else:
+    print("failure")
--- mileage.py
+++ mileage.py
@@ -1,5 +1,4 @@
 print("How many kms did you run today ?")
-km = input() # input result is str by default
-# print(type(km))
-miles = round(float(km)/1.60934, 2)
-print(f"ok input pris pour {km} -> {miles} miles")
+km = input() #request user input
+miles = round(float(km)/1.60934, 2) #input returns a string, need to convert it to float and round it before operations and display
+print(f"Converson de {km} km en miles -> {miles} miles")
